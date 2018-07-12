29c720a78f881fbc590fbb38280b3b07e3d41528
Poonam Jadhav <poonamjadhav@Poonam.hitronhub.home>
2018-07-12 10:15:24 -0700

    feat(delete item):Implemented delete item feature

--- src/components/items.js
+++ src/components/items.js
@@ -3,24 +3,63 @@ import withToken from './withToken'
 import { Button, Divider, List, ListItem, ListItemText, IconButton } from 'material-ui'
 
 class Items extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      items: null
+    }
+    this.updateItems = this.updatedItems.bind(this);
+  }
+
+  deleteItem(id) {
+    const token = sessionStorage.getItem("auth_token");
+    if (this.state) {
+      return fetch('http://localhost:5000/api/todo/'+id, {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+        }
+      })
+      .then(() => this.updateItems(id))
+      .catch(err => {console.log(err); this.props.history.push("/unauthorized")})
+    }
+  }
+
+  updatedItems(id){
+    this.setState({ items: this.state.items.filter(item=>item.id !== id)});
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({ items: nextProps.items});
+  }
+
   render() {
-    const { items } = this.props;
-    return (
-      <div>
-        <h3>To do</h3>
-        <Divider />
-        <List>
-          {
-            items.map(item => (
-              <ListItem key={item.id}>
-                <ListItemText primary={item.name} secondary="Jan 9, 2014" />
-                <Button variant="raised" color="primary" style={{ margin: 10 }}>Edit</Button>
-              </ListItem>
-            ))
-          }
-        </List>
-      </div>
-    )
+    if(this.state.items){
+      const { items } = this.state;
+      return (
+        <div>
+          <h3>To do</h3>
+          <Divider />
+          <List>
+            {
+              items.map(item => (
+                <ListItem key={item.id}>
+                  <ListItemText primary={item.name} secondary="Jan 9, 2014" />
+                  <Button variant="raised" color="primary" style={{ margin: 10 }}>Edit</Button>
+                  <Button variant="raised" color="secondary" style={{ margin: 10 }} onClick={this.deleteItem.bind(this, item.id)}>Delete</Button>
+                </ListItem>
+              ))
+            }
+          </List>
+        </div>
+      )
+    }
+    else{
+      return null
+    }
   }
 }
 
